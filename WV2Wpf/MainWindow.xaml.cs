ae17afb9c9dda75ce78f144afea6cac535526d0b
E33777 <e33777@jp.ibm.com>
2021-02-10 00:04:01 +0900

    Fix.

--- WV2Wpf/MainWindow.xaml.cs
+++ WV2Wpf/MainWindow.xaml.cs
@@ -15,8 +15,11 @@ namespace WV2Wpf
         {
             var webView = new Microsoft.Web.WebView2.Wpf.WebView2();
             var env = await Microsoft.Web.WebView2.Core.CoreWebView2Environment.CreateAsync("C:\\temp\\Microsoft.WebView2.FixedVersionRuntime.88.0.705.56.x64", "c:\\temp\\");
-            await webView.EnsureCoreWebView2Async(env);
-            webView.Source = new System.Uri("https://www.microsoft.com");
+            webView.Loaded += async (sender, e) =>
+            {
+                await webView.EnsureCoreWebView2Async(env);
+                webView.Source = new System.Uri("https://www.microsoft.com");
+            };
 
             var panel = new System.Windows.Controls.DockPanel();
             panel.Children.Add(webView);
