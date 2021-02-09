0a8e103d6a07ded53bb40ac768645b2e4ac6c5e0
E33777 <e33777@jp.ibm.com>
2021-02-09 10:27:40 +0900

    commit samples.

---
+++ WV2Wpf/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// WPF sample.
+/// </summary>
+namespace WV2Wpf
+{
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeAsync();
+        }
+
+        async void InitializeAsync()
+        {
+            var webView = new Microsoft.Web.WebView2.Wpf.WebView2();
+            var env = await Microsoft.Web.WebView2.Core.CoreWebView2Environment.CreateAsync("C:\\temp\\Microsoft.WebView2.FixedVersionRuntime.88.0.705.56.x64", "c:\\temp\\");
+            await webView.EnsureCoreWebView2Async(env);
+            webView.Source = new System.Uri("https://www.microsoft.com");
+
+            var panel = new System.Windows.Controls.DockPanel();
+            panel.Children.Add(webView);
+            Content = panel;
+        }
+    }
+}
